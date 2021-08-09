0c3f07a6d130a3004919567aca991b76103a0cb2
Bryson69 <brysonmundia@gmail.com>
2021-08-09 21:27:57 +0300

    Import Banner

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Banner from '../components/Banner'
 
 // ==== Functional Component ==== //
 export default function Home() {
@@ -10,8 +11,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      {/* Header */}
       <Header />
       {/* Banner */}
+      <Banner />
     </div>
   )
 }
