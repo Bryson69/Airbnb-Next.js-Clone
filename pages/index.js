f120e9861f1be9bed7c22032e70689fb698a7dd9
Bryson69 <brysonmundia@gmail.com>
2021-08-11 01:57:56 +0300

    Add an async function, Import SmallCard Component & Add main tag to display data

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
+import SmallCard from '../components/SmallCard';
 
 // ==== Functional Component ==== //
-export default function Home() {
+export default function Home({exploreData}) {
   return (
     <div className="">
       <Head>
@@ -15,6 +16,37 @@ export default function Home() {
       <Header />
       {/* Banner */}
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6" >
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          {/* Pull data from a server - API Endpoints */}
+          {exploreData?.map(({ img, distance, location }) => (
+            <SmallCard
+            key={img}
+            img={img}
+            distance={distance}
+            location={location}
+             />
+          ))}
+
+        </section>
+      </main>
     </div>
   )
+}
+
+
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp')
+  .then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData
+    }
+  }
 }
