05574ed3deca3ca215bf93d21f8c300b4ac68b39
Bryson69 <brysonmundia@gmail.com>
2021-08-18 13:29:19 +0300

    Add useState to the header component

--- components/Header.js
+++ components/Header.js
@@ -6,8 +6,12 @@ import {
     UserIcon,
     UserCircleIcon
 } from "@heroicons/react/solid"
+import { useState } from "react";
 
 function Header() {
+
+    const [searchInput, setSearchInput] = useState("");
+
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 
         bg-white shadow-md p-5 md:px-10">
@@ -25,7 +29,10 @@ function Header() {
             {/* middle search section */}
             <div className="flex items-center md:border-2 rounded-full
             py-2 md:shadow-sm">
-                <input className="flex-grow pl-5 bg-transparent outline-none text-sm
+                <input
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}
+                className="flex-grow pl-5 bg-transparent outline-none text-sm
                 text-gray-600 placeholder-gray-400" type="text" placeholder="Start your search" />
                 <SearchIcon className=" hidden md:inline-flex h-8 
                 bg-red-400 text-white rounded-full p-2 cursor-pointer
@@ -44,6 +51,9 @@ function Header() {
                     <UserCircleIcon className="h-6" />
                 </div>
             </div>
+
+            
+
         </header>
 
    
