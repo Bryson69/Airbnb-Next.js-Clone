a0b065327d0ead4c63184b518b4731d00a7f2fa3
Bryson69 <brysonmundia@gmail.com>
2021-08-31 19:38:01 +0300

    Add map method

--- pages/search.js
+++ pages/search.js
@@ -2,12 +2,13 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { useRouter } from "next/dist/client/router";
 import { format } from "date-fns";
+import InfoCard from "../components/InfoCard";
 
 function Search({ searchResults }) {
 
     const router = useRouter();
 
-    console.log(searchResults);
+    // console.log(searchResults);
 
     const { location, startDate, endDate, 
     noOfGuests } = router.query;
@@ -33,7 +34,21 @@ function Search({ searchResults }) {
                         <p className="button">Price</p>
                         <p className="button">Rooms and Beds</p>
                         <p className="button">More Filters</p>
+                    </div>
 
+                    <div className="flex flex-col">
+                    {searchResults.map(({ img, location, title, description, star, price, total }) => (
+                        <InfoCard
+                        key={img}
+                        img={img}
+                        location={location}
+                        title={title}
+                        description={description}
+                        star={star}
+                        price={price}
+                        total={total}
+                        />
+                    ))}
                     </div>
                 </section>
             </main>
