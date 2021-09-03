bc9de10efe040783775d701767b21a5140fda3ab
Bryson69 <brysonmundia@gmail.com>
2021-09-03 17:06:03 +0300

    Add mapbox_key

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     images: {
         domains:['links.papareact.com'],
+    },
+    env: {
+        mapbox_key:'pk.eyJ1IjoiYnJ5c29uNjkiLCJhIjoiY2t0M3NidWhoMDNnOTJ4cGw2cWpkc2o4aCJ9.Z0X5RrtfcvxSWwIReWUf1A'
     }
 }
