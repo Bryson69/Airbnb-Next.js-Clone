927278c9a8263ae9b580908a2109700e240f70f3
Bryson69 <brysonmundia@gmail.com>
2021-09-05 18:23:54 +0300

    Add Modify HeroImage

--- components/Banner.js
+++ components/Banner.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import HeroImage from "../img/Hero.jpg";
 
 function Banner() {
     return (
@@ -10,7 +11,7 @@ function Banner() {
             />
 
             <div className="absolute top-1/2 w-full text-center" >
-                <p className="text-sm sm:text-lg" > Not sure where to go? Perfect. </p>
+                <p className="bannerText" > Not sure where to go? Perfect. </p>
                 <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl
                 active:scale-90 transition duration-150 " > 
                 I'm Flexible </button>
@@ -18,5 +19,5 @@ function Banner() {
         </div>
     )
 }
-
+// https://links.papareact.com/0fm
 export default Banner
