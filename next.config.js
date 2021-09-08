4ad75c9bbc4bf2c650bc386109be70bb1d7b5c45
Bryson69 <brysonmundia@gmail.com>
2021-09-08 18:01:32 +0300

    Add image domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     images: {
-        domains:['links.papareact.com'],
+        domains:['links.papareact.com', "a0.muscache.com",],
     },
     env: {
         mapbox_key:'pk.eyJ1IjoiYnJ5c29uNjkiLCJhIjoiY2t0M3NidWhoMDNnOTJ4cGw2cWpkc2o4aCJ9.Z0X5RrtfcvxSWwIReWUf1A'
